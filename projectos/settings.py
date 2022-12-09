03d3612ffcc17e8d0eeed23b0523fc87a0544161
MarieSFloresM <78627002+mariesfloresmoran@users.noreply.github.com>
2022-12-09 14:39:19 -0500

    probando disable collectstatic-setting

--- projectos/settings.py
+++ projectos/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'projectos/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+DISABLE_COLLECTSTATIC = 0
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
